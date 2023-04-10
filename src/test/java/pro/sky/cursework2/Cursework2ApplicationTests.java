6ff878f109d99fc0867166e3dc46c6d37b0f68bf
CosmoBlack <soulker_mozyr@yahoo.com>
2023-04-10 17:44:14 +0200

    Initial commit

---
+++ src/test/java/pro/sky/cursework2/Cursework2ApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.cursework2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cursework2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
